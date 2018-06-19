7b722e65ce7c956fa08acb9dd92603d8ba08b7e6
zenmaldives <39875500+zenmaldives@users.noreply.github.com>
2018-06-19 19:06:39 +0500

    Update example.py

--- example.py
+++ example.py
@@ -24,7 +24,7 @@ bot = InstaBot(
     unfollow_per_day=1000,
     unfollow_break_min=15,
     unfollow_break_max=30,
-    log_mod=1,
+    log_mod=0,
     proxy='',
     # List of list of words, each of which will be used to generate comment
     # For example: "This shot feels wow!"
